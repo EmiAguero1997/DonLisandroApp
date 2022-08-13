f6e9823f66c19ff2fc362e87db5f0294a29967e8
Emiliano <eaguero@folcode.com>
2022-08-13 13:46:21 -0300

    hero component removed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,10 @@
 import Header from "./header";
 import Footer from "./footer";
-import Hero from "./Hero";
 
 export default function Layout(){
     return(
         <>
             <Header/>
-            <Hero/>
             <Footer/>
         </>
     )
