730cfe6f5c0ccc1257ccb0efc3d4a46b6d4802c1
Facundo <fajorezo@hotmail.com>
2022-08-13 16:30:56 -0300

    new component

---
+++ components/Carousel.jsx
@@ -0,0 +1,81 @@
+import Carousel from 'react-material-ui-carousel'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { Grid } from '@mui/material';
+
+const MyCarousel = () => {
+    const data = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        }
+    ]
+
+    const sliderItems = data.length > 3 ? 3 : data.length;
+    const items = [];
+
+    for (let i = 0; i < data.length; i += sliderItems) {
+        if (i % sliderItems === 0) {
+            items.push(
+                <Card raised className="Banner" key={i.toString()}>
+                    <Grid container style={{ display: 'flex', justifyContent: 'space-around', backgroundColor: 'red' }} pacing={0} className="BannerGrid">
+                        {data.slice(i, i + sliderItems).map((index) => {
+                            return (
+                                <Card key={index} sx={{ width: 500, height: 500 }}>
+                                    <CardMedia
+                                        component="img"
+                                        height="100%"
+                                        width="20"
+                                        image="favicon.ico"
+                                        alt="img"
+                                    />
+                                </Card>)
+                        })}
+                    </Grid>
+                </Card>
+            );
+        }
+    }
+
+
+    return (
+        <Carousel animation="slide" autoPlay cycleNavigation timeout={100}>
+            {items}
+        </Carousel>
+    )
+}
+
+export default MyCarousel;
