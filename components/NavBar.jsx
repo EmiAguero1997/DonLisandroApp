7f1d3608a8709cc95c950a524926d9d59eb22101
BuildTools <unconfigured@null.spigotmc.org>
2022-08-13 17:12:04 -0300

    rutas

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,19 +5,34 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/images/logos/DON LISANDRO.png"
+import logo from "../public/images/logos/DON LISANDRO.png";
+import { Button } from "@material-ui/core";
+
 const NavBar = () => {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-            <Typography 
+          <Typography
             noWrap
             component="div"
-            sx={{mr: 2, display: {xs: "none", md: "flex"}}} 
-            >
-                <Image src={logo} width={169} height={100} ></Image>
-            </Typography>
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            <Image src={logo} width={169} height={100}></Image>
+          </Typography>
+
+          <Button>
+            <Link href="/inicio">inicio</Link>
+          </Button>
+          <Button>
+            <Link href="/hostal">hostal</Link>
+          </Button>
+          <Button>
+            <Link href="/windcar">windcar</Link>
+          </Button>
+          <Button>
+            <Link href="/beerpoint">beerpoint</Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
