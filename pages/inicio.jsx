6bf300d2d1467cf6225dfac1e545d55a8d8b2ccc
LMHV <luis.herrero.facultad@gmail.com>
2022-08-13 16:50:09 -0300

    updatev2

--- pages/inicio.jsx
+++ pages/inicio.jsx
@@ -1,6 +1,4 @@
 import Layout from "../components/Layout";
-import Card from '../components/Card'
-import CardHospedaje from "../components/CardHospedaje";
 
 export default function Inicio(){
     return(
